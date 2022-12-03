0a323f283244b9797c5913cf6912e8e9feb761cf
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2022-12-03 11:38:12 +0530

    #include<stdio.h>
    int main()  
    {  
        int n;  
        scanf("%d",&n);  
        for(int i=1;i<=n;i++)  
        {  
            for(int j=1;j<=i;j++)  
            {  
                printf("*");  
            }  
            printf("
    ");  
        }  
        return 0;  
    }

--- printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-int main()
-{
- int n;
- scanf("%d",&n);
- for(int i=1;i<=n;i++)
- {
-    for(int r=1;r<=i;r++)
-    {
-    printf("*");
-    }
-    printf("
-");
- }
- return 0;
-}
+#include<stdio.h>  
+  
+int main()  
+{  
+    int n;  
+    scanf("%d",&n);  
+    for(int i=1;i<=n;i++)  
+    {  
+        for(int j=1;j<=i;j++)  
+        {  
+            printf("*");  
+        }  
+        printf("
+");  
+    }  
+    return 0;  
+}  
