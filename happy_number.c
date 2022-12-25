5ea8c5562f70c7e86aa9582467c9ec6ff460981e
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2022-12-25 22:01:48 +0530

    #include<stdio.h> int hr(int n) {     int r,s=0;     while(n>0)     {         r=n%10;         n=n/10;         s=s+(r*r);     }     return s; } int main() {     int n,m;     scanf("%d",&n);     m=n;    while(m!=1&&m!=4)     {     m=hr(m);     }     if(m==1)     printf("True");     else     printf("False"); }

---
+++ happy_number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int hr(int n)
+{
+    int r,s=0;
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        s=s+(r*r);
+    }
+    return s;
+}
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    m=n;
+   while(m!=1&&m!=4)
+    {
+    m=hr(m);
+    }
+    if(m==1)
+    printf("True");
+    else
+    printf("False");
+}
