7875a106ce707449f5abbd5465b20e00e19be9cf
22P31A0438 <119720806+22P31A0438@users.noreply.github.com>
2023-01-08 17:11:54 +0530

    #include<stdio.h> int man(int n) {     int r,s=0;     while(n>0)     {         r=n%10;         s=s+(r*r);         n=n/10;     }     return s; } int main() {     int n,ram;     scanf("%d",&n);     ram=n;     while(ram!=1&&ram!=4)     {         ram=man(ram);     }     if(ram==1)     printf("True");     else     printf("False");
    }

--- happy_number.c
+++ happy_number.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
-int hr(int n)
+int man(int n)
 {
     int r,s=0;
     while(n>0)
     {
         r=n%10;
-        n=n/10;
         s=s+(r*r);
+        n=n/10;
     }
     return s;
 }
 int main()
 {
-    int n,m;
+    int n,ram;
     scanf("%d",&n);
-    m=n;
-   while(m!=1&&m!=4)
+    ram=n;
+    while(ram!=1&&ram!=4)
     {
-    m=hr(m);
+        ram=man(ram);
     }
-    if(m==1)
+    if(ram==1)
     printf("True");
     else
     printf("False");
+    
 }
